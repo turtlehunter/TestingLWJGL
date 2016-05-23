8b1335476eadf361a4e6d4581fe2f7c24bf00e60
turtlehunter <uriel@urielsalis.me>
2016-05-23 01:29:33 -0300

    1

--- src/main/java/me/urielsalis/game1/base/uncategorized/Main.java
+++ src/main/java/me/urielsalis/game1/base/uncategorized/Main.java
@@ -21,20 +21,60 @@ public class Main {
     private static void gameLoop() {
         while(!Display.isCloseRequested()) {
             glClear(GL_COLOR_BUFFER_BIT); //clear color buffer
+            glLoadIdentity(); //fixes translate
 
             glColor3f(0.25f, 0.75f, 0.5f); //set color (optional, default is white)
+
+            //drawRect(56, 56, 32, 256);
+            drawRect(400, 400, 224, 32);
+
+            Display.update();
+
+        }
+    }
+
+    private static void drawRect(float x, float y, float width, float height) {
+        drawRect(x, y, width, height, 0);
+    }
+
+    private static void drawRectFromCenter(float x, float y, float width, float height, float rot) {
+        glPushMatrix(); //stack. Unique matrix per rectangle so its correct position
+        {
+            width /= 2; //to draw on center
+            height /= 2;
+
+
+            glTranslatef(x, y, 0); //move everything certain amount. 1,0,0 makes funny animation :P
+            glRotatef(rot, 0, 0, 1); //Z always point to camera
+
+            glBegin(GL_QUADS);
+            { //not needed but looks nicer
+                glVertex2f(-width, -height); //replace 0 with negative to draw on center
+                glVertex2f(-width, height); //make points
+                glVertex2f(width, height);
+                glVertex2f(width, -height);
+            }
+            glEnd();
+        }
+        glPopMatrix();
+    }
+
+    private static void drawRect(float x, float y, float width, float height, float rot) {
+        glPushMatrix(); //stack. Unique matrix per rectangle so its correct position
+        {
+            glTranslatef(x, y, 0); //move everything certain amount. 1,0,0 makes funny animation :P
+            glRotatef(rot, 0, 0, 1); //Z always point to camera
 
             glBegin(GL_QUADS);
             { //not needed but looks nicer
                 glVertex2f(0, 0);
-                glVertex2f(0, 64); //make points
-                glVertex2f(64, 64);
-                glVertex2f(64, 0);
+                glVertex2f(0, height); //make points
+                glVertex2f(width, height);
+                glVertex2f(width, 0);
             }
             glEnd();
-
-            Display.update();
         }
+        glPopMatrix();
     }
 
     private static void cleanUp() {
